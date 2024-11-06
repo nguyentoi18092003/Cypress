60ed4a8d808dc8c728d7b87dc7b39a161e4f0547
Nguyen Thi Toi <nguyentoi18092003hvcnbcvt@gmail.com>
2024-11-06 07:21:19 +0700

    Buoi 4: Hoc ve cach run 1 file(chi can thay doi cau lenh trong terminal), hot realod(kieu cu sua roi save no se tu dong chay lai-> phu hop luc viet, viet xong xuoi het r thi su dung yarn test binh thuong thoi)

---
+++ cypress/tests/MutipleMatching.js
@@ -0,0 +1,25 @@
+describe('Run file MutipleMatching',function(){
+    it('Run 1 hehe',function(){
+        cy.visit("/login");
+        //use eq
+         cy.get("input").eq(0).type("tomsmith");// Cai nay no se ra list element, eq(0) lay ra cai phan tu co chi so 0 de tuong tac
+         cy.get("input").eq(1).type("SuperSecretPassword!");// Cai nay no se ra list element, eq(0) lay ra cai phan tu co chi so 0 de tuong tac
+        //cy.get("button[type='submit']").click();
+
+        //Use closure
+        cy.get("input").then(items=>{//items=>{} la arrow function
+            cy.get(items[0]).clear()
+            cy.get(items[0]).type("tomsmith")
+            cy.get(items[1]).clear();
+            cy.get(items[1]).type("SuperSecretPassword!")
+        })
+        //cy.get("button[type='submit']").click();
+
+        //Use .each: duyệt qua mảng
+        cy.get("input").each((item,index)=>{
+            cy.get(item).clear().type("abta");
+        });
+        //cy.get("button[type='submit']").click();
+        cy.wait(5000);
+    });
+});
